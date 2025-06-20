242aef171bb8ffc8a93e1e95f602738271335b19
Caio Pereira <caiopdasilva22@gmail.com>
2025-06-19 22:17:56 -0300

    ajuste estrelas especiais dinamicas

--- script.js
+++ script.js
@@ -81,82 +81,82 @@ function random(range, unit) {
 
 const momentos = [
   {
-    left: 500, top: 750,
+    left: 50, top: 75,
     imgs: ['img/aparecida1.jpg', 'img/aparecida2.jpg', 'img/aparecida3.jpg', 'img/aparecida4.jpg', 'img/aparecida5.jpg'],
     desc: 'Nosso dia em Aparecida ⛪✝️'
   },
   {
-    left: 1100, top: 350,
+    left: 10, top: 35,
     imgs: ['img/brunomars1.jpg', 'img/brunomars2.jpg'],
     desc: 'Bruninho não tem fotos, pq será né? 🤔'
   },
   {
-    left: 100, top: 650,
+    left: 13, top: 65,
     imgs: ['img/capitolio2.jpg', 'img/capitolio1.jpg', 'img/capitolio4.jpg', 'img/capitolio3.jpg', 'img/capitolio5.jpg'],
     desc: 'Capitólio tem nosso coração 💗🏞️',
   },
   {
-    left: 230, top: 600,
+    left: 23, top: 60,
     imgs: ['img/lavrinhas2.jpg', 'img/lavrinhas1.jpg', 'img/lavrinhas3.jpg', 'img/lavrinhas4.jpg'],
     desc: 'Lavrinhas e o Faísca 🏕️🔥',
   },
   {
-    left: 900, top: 500,
+    left: 90, top: 50,
     imgs: ['img/mg2.jpg', 'img/mg1.jpg', 'img/mg3.jpg', 'img/mg4.jpg', 'img/mg5.jpg', 'img/mg6.jpg'],
     desc: 'Minas Gerais, o boi caiu!! 🐂❤️',
   },
   {
-    left: 950, top: 760,
+    left: 95, top: 76,
     imgs: ['img/monteverde1.jpg', 'img/monteverde2.jpg', 'img/monteverde3.jpg', 'img/monteverde4.jpg', 'img/monteverde5.jpg'],
     desc: 'Monte Verde, onde nosso amor floresceu ☀️🏔️',
   },
   {
-    left: 200, top: 400,
+    left: 20, top: 40,
     imgs: ['img/paraibuna1.jpg', 'img/paraibuna2.jpg', 'img/paraibuna3.jpg', 'img/paraibuna4.jpg', 'img/paraibuna5.jpg', 'img/paraibuna6.jpg'],
     desc: 'Paraibuna e nossos dogs, au au 🏞️🐶',
   },
   {
-    left: 500, top: 430,
+    left: 50, top: 43,
     imgs: ['img/parque1.jpg', 'img/parque2.jpg', 'img/parque3.jpg', 'img/parque4.jpg'],
     desc: 'Parque de milhões 🌳❤️',
   },
   {
-    left: 640, top: 625,
+    left: 64, top: 62,
     imgs: ['img/saoroque1.jpg', 'img/saoroque2.jpg', 'img/saoroque3.jpg'],
     desc: 'São Roque e um bom vinho 🍷',
   },
   {
-    left: 1345, top: 525,
+    left: 13, top: 52,
     imgs: ['img/socorro2.jpg', 'img/socorro1.jpg', 'img/socorro3.jpg', 'img/socorro4.jpg', 'img/socorro5.jpg'],
     desc: 'Socorro, uma viagem das boas 🛶❤️',
   },
   {
-    left: 1065, top: 625,
+    left: 85, top: 62,
     imgs: ['img/ubatuba1.jpg', 'img/ubatuba2.jpg', 'img/ubatuba3.jpg', 'img/ubatuba4.jpg'],
     desc: 'Ubatuba, em busca do extraordinário kkkkkk 🏖️🌊',
   },
   {
-    left: 565, top: 325,
+    left: 56, top: 35,
     imgs: ['img/natal1.jpg', 'img/natal2.jpg', 'img/natal3.jpg', 'img/natal4.jpg', 'img/natal5.jpg'],
     desc: 'Natal com a criançada 🎄🎁',
   },
   {
-    left: 1415, top: 245,
+    left: 15, top: 25,
     imgs: ['img/aquiemcasa2.jpg', 'img/aquiemcasa1.jpg', 'img/aquiemcasa3.jpg'],
     desc: 'Uns dias de paz aqui em casa 🏡❤️',
   },
   {
-    left: 895, top: 347,
+    left: 85, top: 47,
     imgs: ['img/euevoce1.jpg', 'img/euevoce2.jpg', 'img/euevoce3.jpg', 'img/euevoce4.jpg', 'img/euevoce5.jpg', 'img/euevoce6.jpg', 'img/euevoce7.jpg', 'img/euevoce8.jpg', 'img/euevoce9.jpg',  'img/euevoce10.jpg',  'img/euevoce11.jpg', 'img/euevoce12.jpg', 'img/euevoce13.jpg', 'img/euevoce14.jpg', 'img/euevoce15.jpg'],
     desc: 'Eu e você, sempre juntos ❤️',
   },
   {
-    left: 395, top: 647,
+    left: 35, top: 67,
     imgs: ['img/show1.jpg', 'img/show2.jpg', 'img/show3.jpg', 'img/show4.jpg', 'img/show5.jpg', 'img/show6.jpg', 'img/show7.jpg', 'img/show8.jpg', 'img/show9.jpg', 'img/show10.jpg'],
     desc: 'Vários shows nos derem muitas boas memórias 🎶',
   },
   {
-    left: 1495, top: 687,
+    left: 55, top: 67,
     imgs: ['img/diadosnamorados1.jpg', 'img/diadosnamorados2.jpg', 'img/diadosnamorados3.jpg'],
     desc: 'Nosso dia dos namorados 1 💕',
   },
@@ -170,11 +170,11 @@ const momentos = [
 function createSpecialStar(momento) {
   let star = document.createElement('div');
   star.classList.add('circle', 'special-star');
-  star.style.left = momento.left + 'px';
-  star.style.top = momento.top + 'px';
-  star.style.width = star.style.height = '20px'; // um pouco maior que as normais
-  star.style.background = '#fffbe6'; // tom branco-amarelado suave
-  star.style.boxShadow = '0 0 10px 3px #fffbe6'; // brilho mais suave
+  star.style.left = momento.left + '%';
+  star.style.top = momento.top + '%';
+  star.style.width = star.style.height = '20px';
+  star.style.background = '#fffbe6';
+  star.style.boxShadow = '0 0 10px 3px #fffbe6';
   star.style.cursor = 'pointer';
   star.addEventListener('click', () => openModal(momento));
   sky.appendChild(star);
